40aba539436a66dad4f2029957781b4ba48add38
Soumyadip Das <dassoumyadip7407@gmail.com>
2025-09-04 02:46:58 +0530

    update CORS config to allow requests from deployed Vercel frontend

--- src/main/java/com/notesapp/notes_backend/security/SecurityConfig.java
+++ src/main/java/com/notesapp/notes_backend/security/SecurityConfig.java
@@ -67,7 +67,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000",
+                "https://notes-frontend-nine-xi.vercel.app" // ✅ deployed frontend
+                ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
